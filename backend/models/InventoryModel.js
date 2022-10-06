afd75de110a2ba45ac6235ef978b3ba8ee08f530
Stephanie DelBelso <sdelbelso@grubhub.com>
2022-10-06 09:31:19 -0400

    Remove Max, Ordered columns, rename Descrepencies

--- backend/models/InventoryModel.js
+++ backend/models/InventoryModel.js
@@ -6,13 +6,11 @@ var ObjectId = mongoose.Types.ObjectId;
 var InventorySchema = new Schema({
   name: { type: String, required: true },
   sku: { type: String, required: true, unique: true },
-  amountOrdered: { type: Number, default: 0 },
   amountAvailable: { type: Number, default: 0 },
   orderStatus: { type: String, default: '' },
   notes: { type: String, default: '' },
   discrepancies: { type: String, default: '' },
   minimum: { type: Number, default: null },
-  maximum: { type: Number, default: null },
 });
 
 InventorySchema.static('findOrCreateItem', function (newItem) {
